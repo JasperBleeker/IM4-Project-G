c08152ff4346665329f42c18fce69f3dfc81fed9
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-04-30 16:08:44 +0200

    Ordnerstruktur
    Bitte Ordnerstruktur beachten.

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var menuButton = document.querySelector(".menu-btn");
-    var mainMenu = document.querySelector(".main-menu");
-  
-    menuButton.addEventListener("click", function() {
-      mainMenu.classList.toggle("show");
-    });
-  });
-  
