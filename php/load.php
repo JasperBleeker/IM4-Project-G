865bd2ea6ec6945cd0068881fc2fd0a5bdc6fd26
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-04-30 18:24:44 +0200

    load erstellt
    load

---
+++ php/load.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "Hello, Load!";
+
+include 'transform.php';
+require_once 'config.php';
+
+//test output
+echo "<pre>";
+print_r($parkingLot);
+echo "</pre>";
+
+try {
+    // Connect to the database
+    $pdo = new PDO($dsn, $db_user, $db_pass, $options);
+    
+    // Define the SQL statement
+    $sql = "INSERT INTO ParkingLots (address, name, state, lot_type, free, total, location) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    // Prepare the SQL statement
+    $stmt = $pdo->prepare($sql);
+
+    foreach ($parkingLot as $lot) {
+        $stmt->execute([
+            ':address' => $lot['address'],
+            ':name' => $lot['name'],
+            ':state' => $lot['state'],
+            ':lot_type' => $lot['lot_type'],
+            ':free' => $lot['free'],
+            ':total' => $lot['total'],
+            ':location' => $lot['location']
+        ]);
+    }
+echo "Data loaded successfully!";
+    
+} catch (PDOException $e) {
+    die("Verbindung zur Datenbank konnte nicht hergestellt werden: " . $e->getMessage());
+}
+
+
+?>
+
