fcda45d1aacf55eb46ab66ec8a5168ee2aa0d462
yurifontanive <133222790+yurifontanive@users.noreply.github.com>
2024-05-02 14:26:05 +0200

    Grundgerüst_HTML5_V1
    V1 Grundgerüst

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+  // Marquee Effect
+  function Marquee(selector, speed) {
+    const parent = document.querySelector(selector);
+    const clone = parent.innerHTML;
+    let i = 0;
+    parent.innerHTML += clone;
+  
+    setInterval(() => {
+      i += speed;
+      if (i >= parent.children[0].clientWidth) i = 0;
+      parent.children[0].style.marginLeft = `-${i}px`;
+    }, 0);
+  }
+  
+  window.addEventListener('load', () => Marquee('.marquee', .3));
