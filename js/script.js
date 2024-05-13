9d03d76cc3706aeb0aa23785d3a7ff74b3659593
yurifontanive <133222790+yurifontanive@users.noreply.github.com>
2024-05-13 11:00:17 +0200

    css

--- js/script.js
+++ js/script.js
@@ -19,25 +19,6 @@ mybtn.addEventListener("click", function () {
    mydiv.classList.toggle("dropdown-div")
 });
 
-
-
-// Marquee Effect
-  function Marquee(selector, speed) {
-    const parent = document.querySelector(selector);
-    const clone = parent.innerHTML;
-    let i = 0;
-    parent.innerHTML += clone;
-  
-    setInterval(() => {
-      i += speed;
-      if (i >= parent.children[0].clientWidth) i = 0;
-      parent.children[0].style.marginLeft = `-${i}px`;
-    }, 0);
-  }
-  
-  window.addEventListener('load', () => Marquee('.marquee', .3));
-
-
 // rotate svg dropdown
 
 let rotated = false;
