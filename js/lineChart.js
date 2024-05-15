95b5c0b54622d0948dc6e9c5e784af5d7eb98791
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-05-15 15:38:59 +0200

    on input suche in JS integriert

--- js/lineChart.js
+++ js/lineChart.js
@@ -1,165 +1,15 @@
-//warten bis das Dokument geladen ist
-document.addEventListener('DOMContentLoaded', () => {
-
-    // API-URL
-    const apiUrl = 'https://project-g.data.dynamicvisualscollective.ch/php/unload.php';
-
-    // Deklarierung der Variablen für alle Funktionen
+(function() {
     let lineChart;
-    let checkedLocations = [];
     let allData = [];
+    let checkedLocations = [];
 
-    // Definition suchfunktion
-    const searchbar = document.getElementById('search');
-
-    // Eventlistener für Start- und Enddatum
-    document.getElementById('startDate').addEventListener('change', updateChartBasedOnDateAndLocation);
-    document.getElementById('endDate').addEventListener('change', updateChartBasedOnDateAndLocation);
-
-    // set the end date to the current date
-    const today = new Date();
-    const currentDate = today.toISOString().slice(0, 10);
-    document.getElementById('endDate').value = currentDate;
-
-    // set the max date to the current date
-    document.getElementById('endDate').max = currentDate;
-    document.getElementById('startDate').max = currentDate;
-
-    
-
-    // Funktion für Chart Farben
+    // Generate a random color for the chart lines
     function getRandomColor() {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         return `rgb(${r},${g},${b})`;
     }
-
-
-    // Fetchen der Daten und Erstellen des Charts
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // console.log('Data fetched:', data);  // Check the fetched data
-            allData = data;
-            createCheckbox(data);
-            initializeChart(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data or processing chart:', error);
-        });
-
-    // Erstellen der Checkboxen
-    function createCheckbox(data) {
-        const locationContainer = document.getElementById('loop-box');
-        locationContainer.innerHTML = ''; // Clear the container before adding new checkboxes
-        const uniqueLocations = new Set();
-        data.forEach(location => {
-            if (!uniqueLocations.has(location.name)) {
-                uniqueLocations.add(location.name);
-
-                const checkboxContainer = document.createElement('div');
-                checkboxContainer.classList.add('loop-check-div');
-
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.classList.add('locationCheckbox');
-                checkbox.name = location.name;
-                checkbox.id = location.name;
-
-                const label = document.createElement('label');
-                label.htmlFor = location.name;
-                label.textContent = location.name;
-
-                checkboxContainer.appendChild(checkbox);
-                checkboxContainer.appendChild(label);
-                locationContainer.appendChild(checkboxContainer);
-            }
-        });
-        // Eventlistener für die Checkboxen
-        attachEventListeners();
-    }
-
-    function attachEventListeners() {
-        // Eventlistener für die Checkboxen
-        // Vom User ausgewählte Standorte für die Anzeige im Chart
-        let locationCheck = document.querySelectorAll('.locationCheckbox');
-
-        locationCheck.forEach(location => {
-            location.addEventListener('change', () => {
-                checkedLocations = Array.from(locationCheck)
-                    .filter(location => location.checked)
-                    .map(location => location.name);
-
-                console.log('Checked locations:', checkedLocations);
-                updateChart(checkedLocations);
-            });
-        });
-    }
-
-    function updateChartBasedOnDateAndLocation() {
-    let selectedLocations = Array.from(document.querySelectorAll('.locationCheckbox'))
-                                 .filter(checkbox => checkbox.checked)
-                                 .map(checkbox => checkbox.name);
-    updateChart(selectedLocations);
-}
-
-
-    // Initialisierung des Charts
-    function updateChart(selectedLocations) {
-        const startDate = new Date(document.getElementById('startDate').value);
-        const endDate = new Date(document.getElementById('endDate').value);
-        endDate.setHours(23, 59, 59, 999); // Set the end date to the end of the day
-
-        const datasets = []; // Array to store the datasets for each location
-
-        selectedLocations.forEach(location => {
-        const filteredData = allData.filter(item => {
-            const itemDate = new Date(item.created);
-            return item.name === location && itemDate >= startDate && itemDate <= endDate;
-        });
-
-        // Map the data for this location to the format required by the chart
-        const dataPoints = filteredData.map(lot => ({
-            x: new Date(lot.created),
-            y: Math.round((lot.total - lot.free) / lot.total * 100), // Assuming lot.total is never 0 @ Jasper
-            free: lot.free,
-            total: lot.total
-        }));
-
-        // Create the dataset for this location
-        const dataset = {
-            label: `${location} Auslastung`,
-            data: dataPoints,
-            borderColor: getRandomColor(),
-            fill: false,
-            tension: 0.1
-        };
-
-        datasets.push(dataset);
-    });
-
-    const ctx = document.getElementById('lineChart').getContext('2d');
-    if (lineChart) {
-        lineChart.destroy(); // Destroy the previous chart before creating a new one
-    }
-
-
-        lineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                datasets: datasets
-                
-            },
-            options: chartOptions  // Defined separately for clarity
-        });
-    }
-
 
     const chartOptions = {
         responsive: true,
@@ -170,9 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 time: {
                     unit: 'hour',
                     displayFormats: {
-                        hour: 'MMM dd, HH'
+                        hour: 'MMM dd, HH:mm'
                     },
-                    tooltipFormat: 'MMM dd, yyyy HH'
+                    tooltipFormat: 'MMM dd, yyyy HH:mm'
                 },
                 title: {
                     display: true,
@@ -184,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 suggestedMax: 100,
                 title: {
                     display: true,
-                    text: 'Auslastung'
+                    text: 'Auslastung (%)'
                 }
             }
         },
@@ -193,7 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 callbacks: {
                     label: function (context) {
                         const dataset = context.dataset;
-                        const index = context.dataIndex;
                         const value = context.raw.y;
                         const free = context.raw.free;
                         const total = context.raw.total;
@@ -204,22 +53,139 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Function to filter parking lots based on user input in the search bar
-    function filterParkingLots() {
-        const searchText = searchbar.value.toLowerCase();
-        const filteredData = allData.filter(parkinglot =>
-            parkinglot.name.toLowerCase().includes(searchText)
-        );
-        createCheckbox(filteredData);
+    function updateChart(selectedLocations) {
+        const startDate = new Date(document.getElementById('startDate').value);
+        const endDate = new Date(document.getElementById('endDate').value);
+        endDate.setHours(23, 59, 59, 999);
+
+        const datasets = [];
+
+        selectedLocations.forEach(location => {
+            const filteredData = allData.filter(item => {
+                const itemDate = new Date(item.created);
+                return item.name === location && itemDate >= startDate && itemDate <= endDate;
+            });
+
+            const dataPoints = filteredData.map(lot => ({
+                x: new Date(lot.created),
+                y: Math.round((lot.total - lot.free) / lot.total * 100),
+                free: lot.free,
+                total: lot.total
+            }));
+
+            const dataset = {
+                label: `${location} Auslastung`,
+                data: dataPoints,
+                borderColor: getRandomColor(),
+                fill: false,
+                tension: 0.1
+            };
+
+            datasets.push(dataset);
+        });
+
+        const ctx = document.getElementById('lineChart').getContext('2d');
+        if (lineChart) {
+            lineChart.destroy();
+        }
+
+        lineChart = new Chart(ctx, {
+            type: 'line',
+            data: { datasets: datasets },
+            options: chartOptions
+        });
     }
 
-    searchbar.addEventListener('input', filterParkingLots);
+    function attachEventListeners() {
+        let locationCheckboxes = document.querySelectorAll('.locationCheckbox');
+        locationCheckboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', () => {
+                checkedLocations = checkedLocations.includes(checkbox.name) ?
+                    checkedLocations.filter(name => name !== checkbox.name) : [...checkedLocations, checkbox.name];
+                updateChart(checkedLocations);
+            });
+        });
+    }
 
+    // Initialize checkboxes from unique location names
+    function initializeCheckboxes(data) {
+        const locationContainer = document.getElementById('loop-box');
+        locationContainer.innerHTML = ''; // Clear previous checkboxes, if any
 
+        // Extract unique locations from data
+        const uniqueLocations = [...new Set(data.map(item => item.name))];
+        
+        uniqueLocations.forEach(locationName => {
+            const checkboxContainer = document.createElement('div');
+            checkboxContainer.classList.add('loop-check-div');
+            checkboxContainer.id = 'div-' + locationName;
 
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.classList.add('locationCheckbox');
+            checkbox.name = locationName;
+            checkbox.id = locationName;
 
-});
+            if (checkedLocations.includes(locationName)) {
+                checkbox.checked = true;
+            }
 
+            const label = document.createElement('label');
+            label.htmlFor = locationName;
+            label.textContent = locationName;
 
+            checkboxContainer.appendChild(checkbox);
+            checkboxContainer.appendChild(label);
+            locationContainer.appendChild(checkboxContainer);
+        });
 
+        attachEventListeners();
+    }
 
+    function filterParkingLots() {
+        const searchbar = document.getElementById('search');
+        const searchText = searchbar.value.toLowerCase();
+        const locationCheckboxes = document.querySelectorAll('.loop-check-div');
+
+        locationCheckboxes.forEach(div => {
+            const checkbox = div.querySelector('input.locationCheckbox');
+            if (searchText === '' || checkbox.name.toLowerCase().includes(searchText)) {
+                div.style.display = ''; // Show the checkbox div
+            } else {
+                div.style.display = 'none'; // Hide the checkbox div
+            }
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const apiUrl = 'https://project-g.data.dynamicvisualscollective.ch/php/unload.php';
+
+        const searchbar = document.getElementById('search');
+        searchbar.addEventListener('input', filterParkingLots);
+
+        document.getElementById('startDate').addEventListener('change', () => updateChart(checkedLocations));
+        document.getElementById('endDate').addEventListener('change', () => updateChart(checkedLocations));
+
+        const today = new Date();
+        const currentDate = today.toISOString().slice(0, 10);
+        document.getElementById('endDate').value = currentDate;
+        document.getElementById('endDate').max = currentDate;
+        document.getElementById('startDate').max = currentDate;
+
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                allData = data;
+                initializeCheckboxes(allData); // Create checkboxes with unique location names
+                updateChart(checkedLocations); // Update chart with initially checked locations
+            })
+            .catch(error => {
+                console.error('Error fetching data or processing chart:', error);
+            });
+    });
+})();
